0a8008614bb4bcf39d23a28d9c27b5f50b3db28c
TeodorPeev <t.peev1@gmail.com>
2018-11-20 14:01:27 +0200

    Added custom exceptions folder

--- WebCasino/WebCasino.Service/Abstract/IUserService.cs
+++ WebCasino/WebCasino.Service/Abstract/IUserService.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebCasino.Entities;
 
 namespace WebCasino.Service.Abstract
 {
     public interface IUserService
     {
-
+        User RetrieveUser(string id);
+        IEnumerable<User> GetAllUsers();
+        User PromoteUser(string id);
+        User LockUser(string id);
+        User EditUser(User user);
     }
 }
---
+++ WebCasino/WebCasino.Service/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebCasino.DataContext;
+using WebCasino.Entities;
+using WebCasino.Service.Abstract;
+
+namespace WebCasino.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly CasinoContext context;
+        public UserService(CasinoContext context)
+        {
+            this.context = context;
+        }
+        public User EditUser(User user)
+        {
+
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User LockUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User PromoteUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User RetrieveUser(string id)
+        {
+            var user = this.context.Users.FirstOrDefault(us => us.Id == id && !us.Locked);
+            if(user == null)
+            {
+
+            }
+            return user;
+        }
+    }
+}
