cc4c1e973a4e63a1441c1ee117c06db2141a4e44
Stefan Veselinov <chofexx@gmail.com>
2018-11-21 13:59:22 +0200

    Adding ServiceValidator with methods implementations

---
+++ WebCasino/WebCasino.Service/Utility/Validator/ServiceValidator.cs
@@ -0,0 +1,62 @@
+﻿namespace WebCasino.Service.Utility.Validator
+{
+	public static class ServiceValidator
+	{
+		public static bool ObjectIsNotEqualNull(object entity)
+		{
+			if (entity != null)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool ValueNotEqualZero(int value)
+		{
+			if (value != 0)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool IsInputStringEmptyOrNull(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool ValueIsBetween(int value, int min, int max)
+		{
+			if (value < min || value > max)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static bool ValueIsBetween(double value, double min, double max)
+		{
+			if (value < min || value > max)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static bool CheckStringLength(int length, int expected)
+		{
+			if (length > expected)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
