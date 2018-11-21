7c43f8685d899791d1577a6845877ad900f6c0b0
TeodorPeev <t.peev1@gmail.com>
2018-11-21 14:07:30 +0200

    Added tests skeleton

--- WebCasino/WebCasino.Service/Abstract/IGameService.cs
+++ WebCasino/WebCasino.Service/Abstract/IGameService.cs
@@ -5,6 +5,6 @@ using System.Text;
 namespace WebCasino.Service.Abstract
 {
     public interface IGameService
-    {
+    { 
     }
 }
---
+++ WebCasino/WebCasino.ServiceTests/TransactionServiceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TransactionServiceTests
+{
+    [TestClass]
+    public class TransactionServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
