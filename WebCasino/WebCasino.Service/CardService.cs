8cb9b1bc7031a099e02ec097a12f1e176d1432e6
Stefan Veselinov <chofexx@gmail.com>
2018-11-22 14:58:24 +0200

    Creating CardService/ CardService Tests
    Implementing ICardService and Tests for ctor

---
+++ WebCasino/WebCasino.Service/CardService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebCasino.DataContext;
+using WebCasino.Entities;
+using WebCasino.Service.Abstract;
+using WebCasino.Service.Utility.Validator;
+
+namespace WebCasino.Service
+{
+	public class CardService : ICardService
+	{
+		private readonly CasinoContext dbContext;
+
+		public CardService(CasinoContext dbContext)
+		{
+			ServiceValidator.ObjectIsNotEqualNull(dbContext);
+
+			this.dbContext = dbContext;
+		}
+
+		public Task<BankCard> AddCard(string cardNumber, string userId, DateTime Expiration)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<BankCard>> GetAllCards(int userId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<BankCard> GetCard(string cardNumber)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<BankCard> RemoveCard(string cardNumber)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<double> Withdraw(string cardNumber)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
