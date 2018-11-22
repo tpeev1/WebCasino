4d18ee2e6f8824785659081bba2526fad2fcae03
Stefan Veselinov <chofexx@gmail.com>
2018-11-22 15:58:33 +0200

    Adding CardService with base implementations of all methods
    Lots of TODO's to find better implementation ( going to fix Interface first)

--- WebCasino/WebCasino.Service/CardService.cs
+++ WebCasino/WebCasino.Service/CardService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebCasino.DataContext;
@@ -21,29 +23,100 @@ namespace WebCasino.Service
 
 		}
 
-		public Task<BankCard> AddCard(string cardNumber, string userId, DateTime Expiration)
+		public async Task<BankCard> AddCard(string cardNumber, string userId, DateTime expiration)
+		{
+			ServiceValidator.IsInputStringEmptyOrNull(cardNumber);
+			ServiceValidator.CheckStringLength(cardNumber, 16, 16);
+			ServiceValidator.IsInputStringEmptyOrNull(userId);
+
+			//TODO: CHOFEXX - VALIDATE DATETIME: IF CURRENT DATE VS EXPIRATION
+			//TODO: CHOFEXX - ADD CREATED ON -> AND TRANSACTION
+			var bankCard = new BankCard()
+			{
+				 CardNumber = cardNumber,
+				  UserId = userId,
+				   Expiration = expiration,
+				   IsDeleted = false,			  
+			};
+
+			await this.dbContext.BankCards.AddAsync(bankCard);
+			await this.dbContext.SaveChangesAsync();
+
+			return bankCard;
+		}
+
+		//TODO: CHOFEXX - FIx validator method ValueNotEqualZero to return <= 0
+		//TODO: CHOFEXX: Fix to async 
+		public async Task<IEnumerable<BankCard>> GetAllCards(string userId)
+		{
+			ServiceValidator.IsInputStringEmptyOrNull(userId);
+
+			var bankCardQuery = this.dbContext.BankCards.Where(u => u.UserId == userId);
+
+			ServiceValidator.ObjectIsNotEqualNull(bankCardQuery);
+
+			return bankCardQuery;
+		}
+
+		//TODO: CHOFEXX: Fix to async 
+		//TODO: Create ValidateCardNumber in validator
+		public async Task<BankCard> GetCard(string cardNumber)
+		{
+			ServiceValidator.IsInputStringEmptyOrNull(cardNumber);
+
+			var bankCardQuery =  this.dbContext.BankCards.Where(c => c.CardNumber == cardNumber).First();
+
+			ServiceValidator.ObjectIsNotEqualNull(bankCardQuery);
+
+			return bankCardQuery;
+		}
+
+		//TODO: CHOFEXX - When card is removed - create transaction to note this remove ?
+		public async Task<BankCard> RemoveCard(string cardNumber)
+		{
+			ServiceValidator.IsInputStringEmptyOrNull(cardNumber);
+
+			var bankCardQuery = this.dbContext.BankCards.Where(c => c.CardNumber == cardNumber).First();
+
+			ServiceValidator.ObjectIsNotEqualNull(bankCardQuery);
+
+			bankCardQuery.IsDeleted = true;
+
+			await this.dbContext.SaveChangesAsync();
+
+			return bankCardQuery;
+		}
+
+		public Task<BankCard> RemoveCard(string cardNumber, double amount)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<IEnumerable<BankCard>> GetAllCards(int userId)
+		//TODO: CHOFEXX - What currency to Withdraw
+		public async Task<double> Withdraw(string cardNumber, double amount)
 		{
-			throw new NotImplementedException();
+			ServiceValidator.IsInputStringEmptyOrNull(cardNumber);
+
+			var bankCardQuery = this.dbContext.BankCards
+				.Where(c => c.CardNumber == cardNumber)
+				.First();
+
+			ServiceValidator.ObjectIsNotEqualNull(bankCardQuery);
+
+			if (bankCardQuery.Expiration.Year <= DateTime.Now.Year &&
+				bankCardQuery.Expiration.Day <= DateTime.Now.Day)
+			{
+				bankCardQuery.MoneyRetrieved += amount;
+				await this.dbContext.SaveChangesAsync();
+			}
+			else
+			{
+				throw new ArgumentNullException();
+			}
+
+			return amount;
 		}
 
-		public Task<BankCard> GetCard(string cardNumber)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<BankCard> RemoveCard(string cardNumber)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<double> Withdraw(string cardNumber)
-		{
-			throw new NotImplementedException();
-		}
+		
 	}
 }
