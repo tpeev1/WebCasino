a3c076d81742246234e90754b0eefc1d85b7e101
Stefan Veselinov <chofexx@gmail.com>
2018-11-23 20:06:09 +0200

    Adding some comments for next level development

--- WebCasino/WebCasino.Service/Utility/APICurrencyGetter/APIGetRequester.cs
+++ WebCasino/WebCasino.Service/Utility/APICurrencyGetter/APIGetRequester.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using WebCasino.Service.Utility.APICurrencyGetter.Configurations;
+using WebCasino.Service.Utility.APICurrencyGetter.Models;
 
 namespace WebCasino.Service.Utility.APICurrencyGetter
 {
-	class HttpClientContext
+	public class APIGetRequester
 	{
+		//TODO: NOTE: Note here that we have just one instance of HttpClient shared 
+		//for the entire application
+		private readonly HttpClient client;
+
+		public APIGetRequester(HttpClient client)
+		{
+			this.client = client ?? throw new ArgumentNullException(nameof(client));
+		}
+
+		//TODO: NOTE THIS COMMENTS FOR NEW MODEL?
+		//public DateTime LastUse { get; set; }
+		//public string Base { get; set; }
+
+		 //TODO: CHOFEXX -> Think about different query params and the way to act on each,
+		 //bottom implementation is basic idea
+		public async Task GetCurrency(string queryParameters = RequestConfig.QueryParameters.LATEST_CURRENCY)
+		{
+			//Validate queryParameters
+			await this.Process(queryParameters);
+		}
+
+		private async Task<CurrencyBindModel> Process(string queryParameters)
+		{
+			var streamTask = await client.GetStringAsync(RequestConfig.REQUEST_URI + queryParameters);
+			//validate getSting
+			var apiRequestResult = JsonConvert.DeserializeObject<CurrencyBindModel>(streamTask);
+			//validate apiRequestResult
+			if (apiRequestResult.Error != null)
+			{
+				//TODO: IDEA: If this api throws exception call Process again with 2-nd API, 
+				//if this doesn't work again call database saved logs
+				throw new ArgumentNullException(apiRequestResult.Error);
+			}
+			return apiRequestResult;
+		}
 	}
 }
--- WebCasino/WebCasino.Service/Utility/APICurrencyGetter/Models/CurrencyBindModel.cs
+++ WebCasino/WebCasino.Service/Utility/APICurrencyGetter/Models/CurrencyBindModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WebCasino.Service.Utility.APICurrencyGetter.Models
 {
-	class CurrencyBindModel
+	public class CurrencyBindModel
 	{
+		[JsonProperty("base")]
+		public string Base { get; set; }
+		[JsonProperty("date")]
+		public string Date { get; set; }
+		[JsonProperty("rates")]
+		public Dictionary<string, decimal> Rates { get; set; }
+
+		[JsonProperty("error")]
+		public string Error { get; set; }
 	}
 }
