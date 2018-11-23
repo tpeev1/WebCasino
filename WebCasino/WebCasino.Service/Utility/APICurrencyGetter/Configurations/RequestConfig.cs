593c2a01acaf1234a058a398f869da521ce37050
Stefan Veselinov <chofexx@gmail.com>
2018-11-23 17:34:25 +0200

    Commenting Consts

--- WebCasino/WebCasino.Service/Utility/APICurrencyGetter/Configurations/RequestConfig.cs
+++ WebCasino/WebCasino.Service/Utility/APICurrencyGetter/Configurations/RequestConfig.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace WebCasino.Service.Utility.APICurrencyGetter.Configurations
 {
-	class RequestConfig
+	public static class RequestConfig
 	{
+		/// <summary>
+		/// MAIN REQUEST URI
+		/// </summary>
+		public const string REQUEST_URI = "https://api.exchangeratesapi.io/";
+
+		/// <summary>
+		/// Add All query parameters for URI
+		/// </summary>
+		public static class QueryParameters
+		{
+			public const string LATEST_CURRENCY = "latest";
+		}
 	}
 }
