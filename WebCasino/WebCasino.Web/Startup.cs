70f155fb8e57447ca120376e175259aca5f8ce0f
Stefan Veselinov <chofexx@gmail.com>
2018-11-30 17:51:22 +0200

    Merging with developer

--- WebCasino/WebCasino.Web/Startup.cs
+++ WebCasino/WebCasino.Web/Startup.cs
@@ -13,64 +13,69 @@ using WebCasino.Web.Utilities.Wrappers;
 
 namespace WebCasino.Web
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<CasinoContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<CasinoContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<CasinoContext>()
-                .AddDefaultTokenProviders();
+			services.AddIdentity<User, IdentityRole>()
+				.AddEntityFrameworkStores<CasinoContext>()
+				.AddDefaultTokenProviders();
 
-            services.AddScoped<IWalletService, WalletService>();
-            services.AddScoped<IUserWrapper, UserWrapper>();
-            services.AddScoped<IUserService, UserService>();
+			services.AddScoped<IWalletService, WalletService>();
+			services.AddScoped<IUserWrapper, UserWrapper>();
+			services.AddScoped<IUserService, UserService>();
 
-            services.AddHttpClient<IHttpWrapper, HttpWrapper>();
+			services.AddHttpClient<IHttpWrapper, HttpWrapper>();
 
-            services.AddSingleton<IAPIRequester, APIRequester>();
+			services.AddSingleton<IAPIRequester, APIRequester>();
 
-            services.AddSingleton<ICurrencyRateApiService, CurrencyRateApiService>();
+			services.AddSingleton<ICurrencyRateApiService, CurrencyRateApiService>();
 
-            // Add application services.
+			// Add application services.
 
-            services.AddMvc();
-        }
+			services.AddMvc();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseStaticFiles();
+			app.UseStaticFiles();
 
-            app.UseAuthentication();
+			app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "admin",
+					template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+				 );
+
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
