2b1fca13ed8f55aa7160a5646fc1506c56e27f45
VeselinovStf <chofexx@gmail.com>
2018-12-06 22:20:50 +0200

    Adding Admin Settings functionality with basic view and controller

---
+++ WebCasino/WebCasino.Web/Areas/Administration/Controllers/AdminSettingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCasino.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    [Authorize(Roles = "Administrator")]
+    public class AdminSettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
